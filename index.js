977ea1f51694e9a55f2d05432bd7f5525d5c3694
shree <email@mail.com>
2021-04-01 23:12:19 +0530

    mongodb init

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 //import circle from "./circle";
 const express = require('express');
 const path = require('path');
+const uniqid = require('uniqid')
 let app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname + 'public')));
+//mongodb stuff
+const { MongoClient } = require("mongodb");
+const uri = 'mongodb://localhost:27017/';
+const client =  new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+//mongodb stuff
 app.post('/hello', (req, res)=>{
     res.send('hello world');
     console.log(req.body.msg);
 })
-
+app.post('/idrec', (req, res)=>{
+    res.send('received id');
+    console.log(req.body.idv);
+})
+app.get('/id', (req, res)=>{
+    res.send(uniqid());
+})
+app.post('/poll', (req, res)=>{
+    console.log('called me');
+    res.send('ok got it')
+})
 app.listen(2000, ()=>{
     console.log('listening at port 2000');
 })
--- public/app.js
+++ public/app.js
@@ -22,3 +22,37 @@ function addhtml(str) {
     let dt = new Date();
     msgele.insertAdjacentHTML('beforeend', `<p class="msgtex">--->[${dt.getHours()} : ${dt.getMinutes()} : ${dt.getSeconds()}] | ${str}</p>`)
 }
+
+
+//getting id
+let idspele = document.querySelector('.idtex');
+async function idgen() {
+    let getid = await fetch('/id');
+    let uid = await getid.text();
+    idspele.innerHTML = uid;
+}
+idgen();
+
+//id element
+async function sendid() {
+    let idval = document.querySelector('.idtexs').value;
+    let obj = {
+        idv : idval
+    }
+    let sendiid = await fetch('/idrec', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+        },
+        body : JSON.stringify(obj),
+    });
+}
+
+
+setInterval(async ()=>{
+    let posreq = await fetch('/poll', {
+        method: 'POST'
+    });
+    console.log(await posreq.text());
+
+}, 1800);
