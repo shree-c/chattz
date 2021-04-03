9fccb73d1f2570ef7bd60d7f8276cd00d59dfd6d
shree <email@mail.com>
2021-04-03 09:02:37 +0530

    .

--- public/app.js
+++ public/app.js
@@ -1,92 +1,95 @@
+//messgae handling function
+let origin = null;
 async function clickhan() {
-    let msgele = document.querySelector('.messagebox');
-    console.log(msgele.value);
-    addhtml(msgele.value);
-    let obj = {
-        msg : msgele.value,
-        id : idspele.innerHTML,
-    }
-    let promise = await fetch('/hello', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(obj),
-    });
-    let text = await promise.text();
-    console.log(text);
-    //promise.then(()=>{console.log(promise.status)});
-    msgele.value = '';
-}
-function addhtml(str) {
-    let msgele = document.querySelector('.msgs');
-    let dt = new Date();
-    msgele.insertAdjacentHTML('beforeend', `<p class="msgtex">--->[${dt.getHours()} : ${dt.getMinutes()} : ${dt.getSeconds()}] | ${str}</p>`)
+  let msgele = document.querySelector(".messagebox");
+  console.log(msgele.value);
+  addhtml(msgele.value);
+  let obj = {
+    msg: msgele.value,
+    id: idspele.innerHTML,
+    sender: origin
+  };
+  let promise = await fetch("/hello", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(obj),
+  });
+  let text = await promise.text();
+  console.log(text);
+  //promise.then(()=>{console.log(promise.status)});
+  msgele.value = "";
 }
 
+function addhtml(str) {
+  let msgele = document.querySelector(".msgs");
+  let dt = new Date();
+  msgele.insertAdjacentHTML(
+    "beforeend",
+    `<p class="msgtex">--->[${dt.getHours()} : ${dt.getMinutes()} : ${dt.getSeconds()}] | ${str}</p>`
+  );
+}
 
 //getting id
-let idspele = document.querySelector('.idtex');
+let idspele = document.querySelector(".idtex");
 async function idgen() {
-    let getid = await fetch('/id');
-    let uid = await getid.text();
-    idspele.innerHTML = uid;
+  let getid = await fetch("/id");
+  let uid = await getid.text();
+  idspele.innerHTML = uid;
+  origin = true;
 }
 idgen();
 
 //id element
 async function sendid() {
-    let idval = document.querySelector('.idtexs').value;
-    let obj = {
-        idv : idval
-    }
-    let sendiid = await fetch('/idrec', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-        },
-        body : JSON.stringify(obj),
-    });
-    const res = await sendiid.text();
-    if (res) {
-        idspele.innerHTML = idval;
-    }
-    console.log(await sendiid.text())
+  let idval = document.querySelector(".idtexs").value;
+  let obj = {
+    idv: idval,
+  };
+  let sendiid = await fetch("/idrec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(obj),
+  });
+  const res = await sendiid.text();
+  if (res) {
+    idspele.innerHTML = idval;
+  }
+  origin = false;
+  console.log(res);
 }
-
-
-setInterval(async ()=>{
-    let obj = {
-        idpoll : idspele.innerHTML,
-    }
-    let posreq = await fetch('/poll', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-        },
-        body : JSON.stringify(obj),
+//pings server with its id and origin value
+setInterval(async () => {
+  let obj = {
+    idpoll: idspele.innerHTML,
+    who: origin,
+  };
+  let posreq = await fetch("/poll", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(obj),
+  });
+  const data = await posreq.json();
+  if (!data) {
+    console.log("null");
+  } else {
+    data.array.forEach((element) => {
+      rechtml(element);
     });
-    const data = await posreq.json();
-    if (!data) {
-        console.log('null')
-    } else {
-        data.array.forEach(element => {
-            rechtml(element)
-        });
-//        console.log(finarr.array);
-//        let obj = JSON.parse(data);
- //       console.log(obj.array);
-//        console.log(finarr[0]);
-    }
-
-
+  }
 }, 1800);
-
 
 //after searching for id, if the search is successfun we will set the main id to the searched one
 function rechtml(arrmes) {
-    let msgele = document.querySelector('.msgs');
-    let dt = new Date();
-    msgele.insertAdjacentHTML('beforeend', `<p class="msgtex"><---[${dt.getHours()} : ${dt.getMinutes()} : ${dt.getSeconds()}] | ${arrmes}</p>`)
-    
+  let msgele = document.querySelector(".msgs");
+  let dt = new Date();
+  msgele.insertAdjacentHTML(
+    "beforeend",
+    `<p class="msgtex"><---[${dt.getHours()} : ${dt.getMinutes()} : ${dt.getSeconds()}] | ${arrmes}</p>`
+  );
 }
--- router.js
+++ router.js
@@ -6,27 +6,39 @@ const db = require('./db');
 const date = new Date();
 //new message handeeler
 router.post('/hello', (req, res)=>{
-    console.log(`ids from messages: ${req.body.id}`)
+    console.log(req.body)
+    //I want to create object in the name of id because I want to identify who is the sender
     if (!global[req.body.id]) {
-    global[req.body.id] = new Array();
-        console.log(`${req.body.id}: ${global[req.body.id][0]} new array created`)
+        global[req.body.id] = {};
+        global[req.body.id].sender = [];
+        global[req.body.id].receiver = [];
+    } else {
+        if (req.body.sender == true) {
+            global[req.body.id].sender.push(req.body.msg);
+        } else {
+            global[req.body.id].receiver.push(req.body.msg);
+        }
     }
-    global[req.body.id].push(req.body.msg);
-    //console.log(`${req.body.id}: ${global[req.body.id][0]} new array created`)
-    res.send("ok got the message")
-    //console.log(req.body.msg);
+    //I am adding the messages to an array having the id of client as name
+    // if (!global[req.body.id]) {
+    // global[req.body.id] = new array();
+    //     console.log(`${req.body.id}: ${global[req.body.id][0]} new array created`)
+    // }
+    // global[req.body.id].push(req.body.msg);
+    // res.send("ok got the message")
 })
 //receiving id 
 router.post('/idrec', async (req, res)=>{
+    //searching whether the id already exists in the database
     let cur = await crud.search(req.body.idv);
     if ( cur ) {
         res.send(true)
     }else {
         res.send(false)
     };
-    console.log(req.body.idv);
+    //console.log(req.body.idv);
 })
-//id creator
+//this is invoked when the client first connects and we push id and its creation timing into the database
 router.get('/id', (req, res)=>{
     const id = uniqid();
     res.send(id);
@@ -35,26 +47,46 @@ router.get('/id', (req, res)=>{
         msgid : id
     }
     crud.insert(insobj).then((added)=>{
-     if (added.insertedCount == 1) {
-        console.log('successfully added to the db');
+    if (added.insertedCount == 1) {
+    //    console.log('successfully added to the db');
     } else {
-        console.log('some error in db insertion');
+    //    console.log('some error in db insertion');
     }
     })
 })
-//polling
+//polling : the client sends the post request every 1.5 seconds with its id and if that id has any pending 
+// messages we would repond it with messages 
+//now the polling happens with who the sender or receiver
 router.post('/poll', (req, res)=>{
-//    console.log('called me');
-    console.log(`from poll: ${req.body.idpoll} : ${global[req.body.idpoll]}`)
-    if (global[req.body.idpoll]) {
-        if (global[req.body.idpoll].length > 0) {
-            console.log(`${global[req.body.idpoll].length} messages pending`)
+    res.status(200);
+    if (req.body.who == true) {
+        if (global[req.body.idpoll]) {
+            if (global[req.body.idpoll].receiver) {
+                console.log(global[req.body.idpoll].receiver)
+                res.json({array : global[req.body.idpoll].receiver})
+                global[req.body.idpoll].receiver  = null;
+            }
         }
-        res.json({array : global[req.body.idpoll]})
-        global[req.body.idpoll] = null;
+    } else  (req.body.who == false)
+    {
+        if (global[req.body.idpoll]) {
+            if (global[req.body.idpoll].sender) {
+                res.json({array : global[req.body.idpoll].sender})
+                global[req.body.idpoll].sender  = null;
+            }
+        }
+    }
+//    console.log('called me');
+//    console.log(`from poll: ${req.body.idpoll} : ${global[req.body.idpoll]}`)
+    // if (global[req.body.idpoll]) {
+    //     if (global[req.body.idpoll].length > 0) {
+    //         console.log(`${global[req.body.idpoll].length} messages pending`)
+    //     }
+    //     res.json({array : global[req.body.idpoll]})
+    //     global[req.body.idpoll] = null;
         
-    } else 
-        res.send(null)
+    // } else 
+        // res.send(null)
 
 })
 module.exports = router
